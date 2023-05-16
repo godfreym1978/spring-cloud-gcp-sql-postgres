3b4111b2b1578801b769401cd13996567055db62
godfreym1978 <godfreym@gmail.com>
2023-05-16 17:14:48 -0500

    initial commit

---
+++ src/main/java/com/example/springcloudgcpsqlpostgres/SqlApplication.java
@@ -0,0 +1,17 @@
+package com.example.springcloudgcpsqlpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Sample application.
+ *
+ * @author João André Martins
+ */
+@SpringBootApplication
+public class SqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/springcloudgcpsqlpostgres/SpringCloudGcpSqlPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudgcpsqlpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGcpSqlPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
