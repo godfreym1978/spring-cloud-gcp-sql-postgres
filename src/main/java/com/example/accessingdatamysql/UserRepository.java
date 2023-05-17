007ffcdff1d2c991f56db70c2795bbd61991cb51
godfreym1978 <godfreym@gmail.com>
2023-05-16 22:01:20 -0500

    Update UserRepository.java

--- src/main/java/com/example/accessingdatamysql/UserRepository.java
+++ src/main/java/com/example/accessingdatamysql/UserRepository.java
@@ -1,7 +1,7 @@
 package com.example.accessingdatamysql;
 
-import org.springframework.data.repository.CrudRepository;
-
+#import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.accessingdatamysql.User;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
