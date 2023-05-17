0f53784d79f1a3d1302218fee8a4d13c40eba117
godfreym1978 <godfreym@gmail.com>
2023-05-16 22:09:46 -0500

    UserRepository changes

--- src/main/java/com/example/accessingdatamysql/UserRepository.java
+++ src/main/java/com/example/accessingdatamysql/UserRepository.java
@@ -7,7 +7,7 @@ import com.example.accessingdatamysql.User;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-@Repository
+
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
