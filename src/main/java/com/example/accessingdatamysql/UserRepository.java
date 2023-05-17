e2c420cb8af74a2ef7995202bfe52840916c0fb8
godfreym1978 <godfreym@gmail.com>
2023-05-16 22:02:56 -0500

    Update UserRepository.java

--- src/main/java/com/example/accessingdatamysql/UserRepository.java
+++ src/main/java/com/example/accessingdatamysql/UserRepository.java
@@ -1,6 +1,6 @@
 package com.example.accessingdatamysql;
 
-#import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.accessingdatamysql.User;
 
